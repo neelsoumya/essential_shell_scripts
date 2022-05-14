68951eafb4607e44238bb6d9c09ddb2c10d33adb
Soumya Banerjee <neel.soumya@gmail.com>
2022-05-14 08:23:07 +0100

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=finished_new.pdf new\ passport-first\ page.pdf  new-passport-last\ page.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=finished.pdf passport_pg1.pdf  passport_pg2.pdf
 
 
