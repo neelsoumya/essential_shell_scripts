707fb2f85c54ca51bd6428661f96b4be43cb4ed2
Soumya Banerjee <neel.soumya@gmail.com>
2022-10-29 20:43:58 +0100

    adding backup shell script

---
+++ backup_script_newmac.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp -r ~/cam_project /Volumes/G-DRIVE\ mobile\ USB/Soumya_new_mac_UK/soumya/
+
+cp -r ~/soumya_cam_mac /Volumes/G-DRIVE\ mobile\ USB/Soumya_new_mac_UK/soumya/
+
+cp -r ~/tcell_ais /Volumes/G-DRIVE\ mobile\ USB/Soumya_new_mac_UK/soumya/
+
+
